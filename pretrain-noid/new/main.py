e4c442f3e7926a330cee9123c3a2c85493e20dfc
ljqjd <131225921+ljqjd@users.noreply.github.com>
2025-09-06 20:40:58 +0800

    Delete pretrain-noid directory

--- pretrain-noid/new/main.py
+++
@@ -1,3 +0,0 @@
-from new import make_dataloader
-train_loader, train_loader_normal, val_loader, corrupted_val_loader, corrupted_query_loader, corrupted_gallery_loader, num_query, num_classes, camera_num, view_num = make_dataloader(cfg)
-
